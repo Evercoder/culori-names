791ca44ffb112980681d21cfcc11b31f0e362590
Dan Burzo <danburzo@gmail.com>
2018-06-15 12:43:02 +0300

    Added Riso color names

---
+++ colors/riso.js
@@ -0,0 +1,318 @@
+// Riso to hex color provided by http://stencil.wiki/colors
+// Released under the Creative Commons Attribution-NonCommercial License
+// https://creativecommons.org/licenses/by-nc/3.0/us/
+
+export default [
+  {
+    "name": "Black",
+    "hex": "#000000"
+  },
+  {
+    "name": "Burgundy",
+    "hex": "#914e72"
+  },
+  {
+    "name": "Blue",
+    "hex": "#0078bf"
+  },
+  {
+    "name": "Green",
+    "hex": "#00a95c"
+  },
+  {
+    "name": "Medium Blue",
+    "hex": "#3255a4"
+  },
+  {
+    "name": "Bright Red",
+    "hex": "#f15060"
+  },
+  {
+    "name": "RisoFederal Blue",
+    "hex": "#3D5588"
+  },
+  {
+    "name": "Purple",
+    "hex": "#765ba7"
+  },
+  {
+    "name": "Teal",
+    "hex": "#00838a"
+  },
+  {
+    "name": "Flat Gold",
+    "hex": "#bb8b41"
+  },
+  {
+    "name": "Hunter Green",
+    "hex": "#407060"
+  },
+  {
+    "name": "Red",
+    "hex": "#ff665e"
+  },
+  {
+    "name": "Brown",
+    "hex": "#925f52"
+  },
+  {
+    "name": "Yellow",
+    "hex": "#ffe800"
+  },
+  {
+    "name": "Marine Red",
+    "hex": "#d2515e"
+  },
+  {
+    "name": "Orange",
+    "hex": "#ff6c2f"
+  },
+  {
+    "name": "Fluorescent Pink",
+    "hex": "#ff48b0"
+  },
+  {
+    "name": "Light Gray",
+    "hex": "#88898a"
+  },
+  {
+    "name": "Metallic Gold",
+    "hex": "#ac936e"
+  },
+  {
+    "name": "Crimson",
+    "hex": "#e45d50"
+  },
+  {
+    "name": "Fluorescent Orange",
+    "hex": "#ff7477"
+  },
+  {
+    "name": "Cornflower",
+    "hex": "#62a8e5"
+  },
+  {
+    "name": "Sky Blue",
+    "hex": "#4982cf"
+  },
+  {
+    "name": "Sea Blue",
+    "hex": "#0074a2"
+  },
+  {
+    "name": "Lake",
+    "hex": "#235ba8"
+  },
+  {
+    "name": "Indigo",
+    "hex": "#484d7a"
+  },
+  {
+    "name": "Midnight",
+    "hex": "#435060"
+  },
+  {
+    "name": "Mist",
+    "hex": "#d5e4c0"
+  },
+  {
+    "name": "Granite",
+    "hex": "#a5aaa8"
+  },
+  {
+    "name": "Charcoal",
+    "hex": "#70747c"
+  },
+  {
+    "name": "Smoky Teal",
+    "hex": "#5f8289"
+  },
+  {
+    "name": "Steel",
+    "hex": "#375e77"
+  },
+  {
+    "name": "Slate",
+    "hex": "#5e695e"
+  },
+  {
+    "name": "Turquoise",
+    "hex": "#00aa93"
+  },
+  {
+    "name": "Emerald",
+    "hex": "#19975d"
+  },
+  {
+    "name": "Grass",
+    "hex": "#397e58"
+  },
+  {
+    "name": "Forest",
+    "hex": "#516e5a"
+  },
+  {
+    "name": "Spruce",
+    "hex": "#4a635d"
+  },
+  {
+    "name": "Moss",
+    "hex": "#68724d"
+  },
+  {
+    "name": "Sea Foam",
+    "hex": "#62c2b1"
+  },
+  {
+    "name": "Kelly Green",
+    "hex": "#67b346"
+  },
+  {
+    "name": "Light Teal",
+    "hex": "#009da5"
+  },
+  {
+    "name": "Ivy",
+    "hex": "#169b62"
+  },
+  {
+    "name": "Pine",
+    "hex": "#237e74"
+  },
+  {
+    "name": "Lagoon",
+    "hex": "#2f6165"
+  },
+  {
+    "name": "Violet",
+    "hex": "#9d7ad2"
+  },
+  {
+    "name": "Orchid",
+    "hex": "#aa60bf"
+  },
+  {
+    "name": "Plum",
+    "hex": "#845991"
+  },
+  {
+    "name": "Raisin",
+    "hex": "#775d7a"
+  },
+  {
+    "name": "Grape",
+    "hex": "#6c5d80"
+  },
+  {
+    "name": "Scarlet",
+    "hex": "#f65058"
+  },
+  {
+    "name": "Tomato",
+    "hex": "#d2515e"
+  },
+  {
+    "name": "Cranberry",
+    "hex": "#d1517a"
+  },
+  {
+    "name": "Maroon",
+    "hex": "#9e4c6e"
+  },
+  {
+    "name": "Raspberry Red",
+    "hex": "#d1517a"
+  },
+  {
+    "name": "Brick",
+    "hex": "#a75154"
+  },
+  {
+    "name": "Light Lime",
+    "hex": "#e3ed55"
+  },
+  {
+    "name": "Sunflower",
+    "hex": "#ffb511"
+  },
+  {
+    "name": "Melon",
+    "hex": "#ffae3b"
+  },
+  {
+    "name": "Apricot",
+    "hex": "#f6a04d"
+  },
+  {
+    "name": "Paprika",
+    "hex": "#ee7f4b"
+  },
+  {
+    "name": "Pumpkin",
+    "hex": "#ff6f4c"
+  },
+  {
+    "name": "Bright Olive Green",
+    "hex": "#b49f29"
+  },
+  {
+    "name": "Bright Gold",
+    "hex": "#ba8032"
+  },
+  {
+    "name": "Copper",
+    "hex": "#bd6439"
+  },
+  {
+    "name": "Mahogany",
+    "hex": "#8e595a"
+  },
+  {
+    "name": "Bisque",
+    "hex": "#f2cdcf"
+  },
+  {
+    "name": "Bubble Gum",
+    "hex": "#f984ca"
+  },
+  {
+    "name": "Light Mauve",
+    "hex": "#e6b5c9"
+  },
+  {
+    "name": "Dark Mauve",
+    "hex": "#bd8ca6"
+  },
+  {
+    "name": "Wine",
+    "hex": "#914e72"
+  },
+  {
+    "name": "Gray",
+    "hex": "#928d88"
+  },
+  {
+    "name": "White",
+    "hex": "#FFFFFF"
+  },
+  {
+    "name": "Aqua",
+    "hex": "#5ec8e5"
+  },
+  {
+    "name": "Mint",
+    "hex": "#82d8d5"
+  },
+  {
+    "name": "Fluorescent Yellow",
+    "hex": "#ffe900"
+  },
+  {
+    "name": "Fluorescent Red",
+    "hex": "#FF4C65"
+  },
+  {
+    "name": "Fluorescent Green",
+    "hex": "#44d62c"
+  }
+]
